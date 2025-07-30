ffd126f7824e7330f36a440fc112153fc03908e4
K <kareem.m.dev@gmail.com>
2025-07-30 11:02:48 +0000

    Finished line reading logic

--- src/main.c
+++ src/main.c
@@ -1,8 +1,51 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void ksh_loop(void) {
+#define KSH_RL_BUFSIZE 1024
+char *ksh_read_line(void)
+{
+    int bufsize = KSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * KSH_RL_BUFSIZE);
+    int c;
 
+    if (!buffer)
+    {
+        fprintf(stderr, "lsh: error allocation\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+    {
+        c = getchar();
+
+        if (c == EOF || c == '\n')
+        {
+            buffer[position] = '\0';
+            return buffer;
+        }
+        else
+        {
+            buffer[position] = c;
+        }
+        position++;
+
+        if (position >= bufsize)
+        {
+            bufsize += KSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+
+            if (!buffer)
+            {
+                fprintf(stderr, "lsh: error allocation\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+void ksh_loop(void)
+{
 }
 
 int main(int argc, char **argv)
